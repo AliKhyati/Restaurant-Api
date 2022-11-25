16023d2bc04d4decf6e9299a9161d227433f158b
alikhyati <alikhyatihotmail.com>
2022-11-25 11:45:41 +0100

    convert reservation from json-server to api spring

--- src/main/java/projet/wcs/starter/controllers/ReservationController.java
+++ src/main/java/projet/wcs/starter/controllers/ReservationController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.*;
 import projet.wcs.starter.dao.Reservation;
 import projet.wcs.starter.dto.ReservationDto;
 import projet.wcs.starter.repositories.ReservationRepository;
+import projet.wcs.starter.repositories.RestaurantTableRepository;
 import projet.wcs.starter.services.CommandService;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,23 +27,37 @@ public class ReservationController {
 
     @Autowired private CommandService commandService;
 
+    @Autowired private RestaurantTableRepository tableRepository;
+
     @GetMapping
-    @PreAuthorize("hasRole('USER')")
     public List<ReservationDto> getAll() {
         return repo.findAll().stream().map(
                 reservation -> modelMapper.map(reservation, ReservationDto.class)
         ).collect(Collectors.toList());
     }
 
-    @PostMapping
-    public ReservationDto create(@RequestBody @Valid ReservationDto reservation) {
+    @PostMapping("/{date}")
+    public ReservationDto create(@PathVariable String date, @RequestBody @Valid ReservationDto reservation) throws ParseException {
         reservation.setCommand(commandService.create());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        Date formatedDate = simpleDateFormat.parse(date);
+        reservation.setDate(formatedDate);
         Reservation savedReservation = repo.save(modelMapper.map(reservation, Reservation.class));
         return modelMapper.map(savedReservation, ReservationDto.class);
     }
 
-    @DeleteMapping
-    public boolean delete(@RequestParam Integer id) {
+    @GetMapping("/get/{date}")
+    public List<ReservationDto> getReservationsByDate(@PathVariable String date) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        Date formatedDate = simpleDateFormat.parse(date);
+        System.out.println(date);
+        return repo.findByDate(formatedDate).stream().map(
+                reservation -> modelMapper.map(reservation, ReservationDto.class)
+        ).collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Integer id) {
         if (id != null) {
             repo.deleteById(id);
             return true;
